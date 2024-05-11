bdf40c2557fca1213e408dfc353d179917269e14
Anwar Hossain Mokhter <anwar.exenso@gmail.com>
2024-05-11 13:23:14 +0600

    WS2812 LEDs using SPI with STM32

---
+++ Tutorial 18/WS2812LED/Core/Inc/WS2812_SPI.h
@@ -0,0 +1,16 @@
+/*
+ * WS2812_SPI.h
+ *
+ *  Created on: May 10, 2024
+ *      Author: Anwar
+ */
+
+#ifndef INC_WS2812_SPI_H_
+#define INC_WS2812_SPI_H_
+
+
+void setLED (int led, int RED, int GREEN, int BLUE);
+void WS2812_Send (void);
+
+
+#endif /* INC_WS2812_SPI_H_ */
